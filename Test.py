870ce47f4a2b32b242c041445cbdb41c7be55f06
Arnaud KOPP <kopp.arnaud@gmail.com>
2015-04-22 16:13:23 +0200

    PlateAnalysis don't crash without positive control

--- Test.py
+++ Test.py
@@ -19,12 +19,12 @@ np.set_printoptions(suppress=True, precision=4)
 """
 Go Enrichment testing
 """
-enrichment = HTSDataMining.EnrichmentStudy(study="/home/arnaud/Desktop/HDV/GO_enrichement/union_study_poc.txt",
-                                           pop="/home/arnaud/Desktop/HDV/GO_enrichement/union_pop.txt",
-                                           assoc="/home/arnaud/Desktop/asso_gene_genome_goid.csv",
+enrichment = HTSDataMining.EnrichmentStudy(study="/home/arnaud/Desktop/HDV/DATA/GO_enrichement/study.txt",
+                                           pop="/home/arnaud/Desktop/HDV/DATA/GO_enrichement/pop8.txt",
+                                           assoc="/home/arnaud/Desktop/TEMP/asso_gene_genome_goid.csv",
                                            compare=False,
                                            namespace_filter=None)
 result = enrichment.to_dataframe()
 df = pd.DataFrame(result)
 print(df.head())
-df.to_csv(path_or_buf='/home/arnaud/Desktop/HDV/GO_enrichement/union_GO.csv', index=False)
+df.to_csv(path_or_buf='/home/arnaud/Desktop/HDV/DATA/GO_enrichement/GO_enrichment_8.csv', index=False)
