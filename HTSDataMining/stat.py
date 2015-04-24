6033deb8ad281236788b8afac8b7f90456f43d8b
Arnaud KOPP <kopp.arnaud@gmail.com>
2015-04-24 11:00:47 +0200

    Remove version statement

--- HTSDataMining/stat.py
+++ HTSDataMining/stat.py
@@ -9,7 +9,6 @@ __author__ = "Arnaud KOPP"
 __copyright__ = "© 2014-2015 KOPP Arnaud All Rights Reserved"
 __credits__ = ["KOPP Arnaud"]
 __license__ = "GNU GPL V2.0"
-__version__ = "1.0"
 __maintainer__ = "Arnaud KOPP"
 __email__ = "kopp.arnaud@gmail.com"
 __status__ = "Production"
